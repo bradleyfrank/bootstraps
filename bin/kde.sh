7794a7b5ddcbe919957392b45a2a1d130af2350f
Brad <bradley.frank@gmail.com>
2019-02-13 14:40:43 -0500

    Fixes syntax and comments.

--- bin/kde.sh
+++ bin/kde.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# set default terminal profile
-kwriteconfig5 --file konsolerc --group 'Desktop Entry' --key DefaultProfile 'Brads-Terminal.profile'
+# default terminal profile
+kwriteconfig5 --file konsolerc --group "Desktop Entry" --key "DefaultProfile" "Brads-Terminal.profile"
 
-# set Dolphin click behavior to double-click
-kwriteconfig5 --file kdeglobals --group KDE --key SingleClick 'false'
+# interaction behavior is double-click
+kwriteconfig5 --file kdeglobals --group "KDE" --key "SingleClick" "false"
 
-# set caps lock to control
-kwriteconfig5 --file kxkbrc --group Layout --key Options 'ctrl:nocaps'
+# caps lock is control
+kwriteconfig5 --file kxkbrc --group "Layout" --key "Options" "ctrl:nocaps"
