855589466596bf49c4a31944bb768b4d53766397
Brad <bradley.frank@gmail.com>
2019-02-13 14:43:49 -0500

    Adds configs for key repeat rate and desktop theme.

--- bin/kde.sh
+++ bin/kde.sh
@@ -8,3 +8,10 @@ kwriteconfig5 --file kdeglobals --group "KDE" --key "SingleClick" "false"
 
 # caps lock is control
 kwriteconfig5 --file kxkbrc --group "Layout" --key "Options" "ctrl:nocaps"
+
+# increase key repeat rates
+kwriteconfig5 --file kcminputrc --group "Keyboard" --key "RepeatDelay" 140
+kwriteconfig5 --file kcminputrc --group "Keyboard" --key "RepeatRate" 50.00
+
+# make Breeze default desktop theme
+kwriteconfig5 --file plasmarc --group "Theme" --key "name" "default"
