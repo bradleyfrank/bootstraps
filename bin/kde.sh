d0b8591a860f5b45299e0dc6265d02d3340df932
Brad <bradley.frank@gmail.com>
2019-02-12 22:32:15 -0500

    Adds config to make caps lock a control key.

--- bin/kde.sh
+++ bin/kde.sh
@@ -5,3 +5,6 @@ kwriteconfig5 --file konsolerc --group 'Desktop Entry' --key DefaultProfile 'Bra
 
 # set Dolphin click behavior to double-click
 kwriteconfig5 --file kdeglobals --group KDE --key SingleClick 'false'
+
+# set caps lock to control
+kwriteconfig5 --file kxkbrc --group Layout --key Options 'ctrl:nocaps'
