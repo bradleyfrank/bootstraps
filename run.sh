b45822dcc99f2917ace4e5479d56cdb700e3a256
Brad <bradley.frank@gmail.com>
2019-06-25 14:31:40 -0400

    Auto accept repo enable.

--- run.sh
+++ run.sh
@@ -75,7 +75,7 @@ bootstrap_fedora() {
     https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$os_majver".noarch.rpm
   sudo dnf config-manager --add-repo=https://negativo17.org/repos/fedora-spotify.repo
   sudo dnf config-manager --set-enabled google-chrome
-  sudo dnf copr enable dawid/better_fonts
+  sudo dnf copr enable dawid/better_fonts -y
 
   # update and install packages
   sudo dnf clean all
