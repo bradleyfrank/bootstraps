1f6461a29da9b3dd295932312af19c6cc4033396
Brad <bradley.frank@gmail.com>
2019-11-27 16:52:45 -0500

    Fix git clone command.

--- playbook.sh
+++ playbook.sh
@@ -6,7 +6,7 @@ bootstrap_repo="https://github.com/bradleyfrank/bootstraps.git"
 tmp_repo="$(mktemp -d)"
 
 sudo dnf install ansible git -y
-git_clone_repo "$tmp_repo" "$bootstrap_repo"
+git clone "$bootstrap_repo" "$tmp_repo"
 pushd "$tmp_repo" >/dev/null 2>&1 || exit 1
 git checkout ansible || exit 1
 ansible-playbook site.yml --ask-vault-pass --ask-become-pass
