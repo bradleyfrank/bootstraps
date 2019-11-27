4bb88d29cd6766b921978d4b67aac9afb392d703
Brad <bradley.frank@gmail.com>
2019-11-27 16:47:04 -0500

    Specify ansible branch.

--- playbook.sh
+++ playbook.sh
@@ -8,6 +8,7 @@ tmp_repo="$(mktemp -d)"
 sudo dnf install ansible git -y
 git_clone_repo "$tmp_repo" "$bootstrap_repo"
 pushd "$tmp_repo" >/dev/null 2>&1 || exit 1
+git checkout ansible || exit 1
 ansible-playbook site.yml --ask-vault-pass --ask-become-pass
 popd >/dev/null 2>&1 || exit 1
 
