5f02728850ffed74e11cf3c7e86042ab805f69b5
Brad <bradley.frank@gmail.com>
2019-11-27 16:42:31 -0500

    First pass at full Ansible bootstrap.

---
+++ playbook.sh
@@ -0,0 +1,14 @@
+#!/usr/local/env bash
+
+set -eu
+
+bootstrap_repo="https://github.com/bradleyfrank/bootstraps.git"
+tmp_repo="$(mktemp -d)"
+
+sudo dnf install ansible git -y
+git_clone_repo "$tmp_repo" "$bootstrap_repo"
+pushd "$tmp_repo" >/dev/null 2>&1 || exit 1
+ansible-playbook site.yml --ask-vault-pass --ask-become-pass
+popd >/dev/null 2>&1 || exit 1
+
+exit 0
