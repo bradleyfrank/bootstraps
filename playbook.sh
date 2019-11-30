e988edaac19568a615bdf87b56e718730c198d0e
Brad <bradley.frank@gmail.com>
2019-11-30 00:04:32 -0500

    Continue adding MacOS support.

--- playbook.sh
+++ playbook.sh
@@ -2,11 +2,28 @@
 
 set -eu
 
+homebrew_url="https://raw.githubusercontent.com/Homebrew/install/master/install"
 bootstrap_repo="https://github.com/bradleyfrank/bootstraps.git"
 tmp_repo="$(mktemp -d)"
 
-sudo dnf install ansible git -y
+case "$OSTYPE" in
+  "darwin"*) __os="macos" ;;
+   "linux"*) __os="linux" ;;
+          *) printf '%s\n' "Unsupported OS, aborting..." >&2 ; exit 1 ;;
+esac
+
+if [[ "$__os" == "macos" ]]; then
+  xcode-select --install
+  [[ ! type brew >/dev/null 2>&1 ]] && /usr/bin/ruby -e "$(curl -fsSL "$homebrew_url")"
+  brew update
+  brew upgrade
+  brew install ansible git
+elif [[ "$__os" == "linux" ]]; then
+  sudo dnf install ansible git -y
+fi
+
 git clone "$bootstrap_repo" "$tmp_repo"
+
 pushd "$tmp_repo" >/dev/null 2>&1 || exit 1
 git checkout ansible || exit 1
 ansible-playbook site.yml --ask-vault-pass --ask-become-pass
