f76047f66c44030c9d08e09cf5c9968869b9c8d3
Brad <bradley.frank@gmail.com>
2019-12-18 11:08:24 -0500

    Use ansible-pull instead of Git; put Git into package install list.

--- run.sh
+++ run.sh
@@ -20,20 +20,20 @@ trap cleanup EXIT
 if [[ $OSTYPE =~ ^darwin ]]; then
   xcode-select --install
   [[ ! -x /usr/local/bin/brew ]] && /usr/bin/ruby -e "$(curl -fsSL "$homebrew_url")"
-  brew install ansible git
+  brew install ansible
 elif [[ $OSTYPE =~ ^linux ]]; then
   case "$(sed -rn 's/^ID=([a-z]+)/\1/p' /etc/os-release)" in
-    fedora) sudo dnf install ansible git -y ;;
-         *) not_supported                   ;;
+    fedora) sudo dnf install ansible -y ;;
+         *) not_supported               ;;
   esac
 else
   not_supported
 fi
 
-git clone "$bootstrap_repo" "$tmp_repo"
-
-pushd "$tmp_repo" >/dev/null 2>&1 || exit 1
-ansible-playbook site.yml --ask-become-pass --vault-id @prompt
-popd >/dev/null 2>&1 || exit 1
+ansible-pull \
+  --url "$bootstrap_repo" \
+  --directory "$tmp_repo" \
+  --ask-become-pass --vault-id @prompt \
+  site.yml
 
 exit 0
