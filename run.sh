d07647aa567b9eccf9a14893c89a126fae683a5c
Bradley Frank <bradfrank@fastmail.com>
2020-04-28 09:34:05 -0400

    Break up monolithic role into constituent parts.

--- run.sh
+++ run.sh
@@ -3,37 +3,44 @@
 homebrew_url="https://raw.githubusercontent.com/Homebrew/install/master/install"
 bootstrap_repo="https://github.com/bradleyfrank/bootstraps.git"
 
-tmp_repo="$(mktemp -d)"
-
-cleanup() {
-  rm -rf "$tmp_repo"
-}
-
 not_supported() {
   printf '%s\n' "Unsupported OS, aborting..." >&2
   exit 1
 }
 
-trap cleanup EXIT
-
-if [[ $OSTYPE =~ ^darwin ]]; then
+bootstrap_macos() {
   xcode-select --print-path &> /dev/null || xcode-select --install
   [[ ! -x /usr/local/bin/brew ]] && /usr/bin/ruby -e "$(curl -fsSL "$homebrew_url")"
   [[ ! -x /usr/local/bin/ansible ]] && brew install ansible git
-elif [[ $OSTYPE =~ ^linux ]]; then
+}
+
+bootstrap_linux() {
   case "$(sed -rn 's/^ID=([a-z]+)/\1/p' /etc/os-release)" in
     fedora) sudo dnf install ansible git -y ;;
          *) not_supported                   ;;
   esac
-else
-  not_supported
-fi
+}
 
-ansible-pull \
+case "$OSTYPE" in
+  ^darwin) bootstrap_macos ;;
+   ^linux) bootstrap_linux ;;
+        *) not_supported   ;;
+esac
+
+tmp_repo="$(mktemp -d)"
+
+if ansible-pull \
   --url "$bootstrap_repo" \
   --directory "$tmp_repo" \
   --purge \
-  --ask-become-pass --vault-id @prompt \
-  bootstrap.yml
-
-exit 0
+  --ask-become-pass \
+  --vault-id @prompt \
+  playbooks/"$(echo "$OSTYPE" | grep -Eo "(darwin|linux)")".yml
+then
+  rm -rf "$tmp_repo"
+  exit 0
+else
+  mv "$tmp_repo" "$HOME"
+  echo "Ansible run failed; repo saved to $HOME"
+  exit 1
+fi
