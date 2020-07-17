b18fb9fba4ec6ad3000a12eb340dd4b7b8c84a9e
Bradley Frank <bradfrank@fastmail.com>
2020-07-17 15:44:04 -0400

    Make sure Homebrew is installed before loading completions.

--- roles/dotfiles/files/dotfiles/.config/shellrc.d/completions.zsh
+++ roles/dotfiles/files/dotfiles/.config/shellrc.d/completions.zsh
@@ -1,4 +1,4 @@
-FPATH="$(brew --prefix)/share/zsh-completions":$FPATH
+type brew &> /dev/null && FPATH="$(brew --prefix)/share/zsh-completions":$FPATH
 
 zstyle ':completion:*' list-suffixes expand prefix suffix
 zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]}={[:upper:][:lower:]}' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*' 'm:{[:lower:][:upper:]}={[:upper:][:lower:]} l:|=* r:|=*'
