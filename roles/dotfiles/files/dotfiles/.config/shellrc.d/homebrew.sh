8040f186e26d8c1882c556d21f85e154b72a5f1c
Bradley Frank <bradfrank@fastmail.com>
2020-07-17 13:34:58 -0400

    Add dotfiles script for applying tags.

--- roles/dotfiles/files/dotfiles/.config/shellrc.d/homebrew.sh
+++ roles/dotfiles/files/dotfiles/.config/shellrc.d/homebrew.sh
@@ -1,4 +1,6 @@
-export HOMEBREW_PREFIX="$(brew --prefix)"
-export HOMEBREW_CELLAR="$(brew --prefix)/Cellar"
-export HOMEBREW_REPOSITORY="$(brew --prefix)/Homebrew"
-export INFOPATH="$(brew --prefix)/share/info:${INFOPATH:-}"
+if type brew &> /dev/null; then
+  export HOMEBREW_PREFIX="$(brew --prefix)"
+  export HOMEBREW_CELLAR="$(brew --prefix)/Cellar"
+  export HOMEBREW_REPOSITORY="$(brew --prefix)/Homebrew"
+  export INFOPATH="$(brew --prefix)/share/info:${INFOPATH:-}"
+fi
--- run.sh
+++ run.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 
-homebrew_url="https://raw.githubusercontent.com/Homebrew/install/master/install"
-bootstrap_repo="https://github.com/bradleyfrank/bootstraps.git"
-dotfiles_dir="$HOME"/.config/dotfiles
-system_os="$(uname -s | tr '[:upper:]' '[:lower:]')"
-
 not_supported() {
-  printf '%s\n' "Unsupported OS, aborting..." >&2
+  printf "%s\n" "Unsupported OS, aborting..." >&2
   exit 1
 }
 
 bootstrap_macos() {
+  homebrew_url="https://raw.githubusercontent.com/Homebrew/install/master/install"
   xcode-select --print-path >/dev/null 2>&1 || xcode-select --install
   [ ! -x /usr/local/bin/brew ] && /usr/bin/ruby -e "$(curl -fsSL "$homebrew_url")"
   [ ! -x /usr/local/bin/ansible ] && brew install ansible git
@@ -24,11 +20,15 @@ bootstrap_linux() {
 }
 
 main() {
+  ansible_repo="https://github.com/bradleyfrank/bootstraps.git"
+  system_os="$(uname -s | tr '[:upper:]' '[:lower:]')"
+  tmp_checkout="$(mktemp -d)"
+
   while getopts ':wh' flag; do
     case "${flag}" in
       w) skip_tags="home_only" ;;
       h) skip_tags="work_only" ;;
-      *) printf '%s\n' "Requires [-w|-h], aborting..." >&2 ; exit 1 ;;
+      *) printf "%s\n" "Requires [-w|-h], aborting..." >&2 ; exit 1 ;;
     esac
   done
 
@@ -38,20 +38,18 @@ main() {
          *) not_supported   ;;
   esac
 
-  [ ! -d "$dotfiles_dir" ] && mkdir -p "$dotfiles_dir"
-
   if ansible-pull \
-    --url "$bootstrap_repo" \
-    --directory "$dotfiles_dir" \
-    --purge \
+    --url "$ansible_repo" \
+    --directory "$tmp_checkout" \
     --ask-become-pass \
     --skip-tags "$skip_tags" \
     playbooks/"$system_os".yml
   then
+    rm -rf "$tmp_checkout"
     exit 0
   else
-    rm -rf "$dotfiles_dir"
-    printf '%s\n' "Ansible run failed" >&2
+    rm -rf "$tmp_checkout"
+    printf "%s\n" "Ansible run failed" >&2
     exit 1
   fi
 }
